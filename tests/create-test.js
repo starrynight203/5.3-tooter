c4b05608efa180f5bc151996a37c79b37cf11260
Hannah Smith <hannahsmith@Hannahs-MacBook-Pro.local>
2016-03-03 09:38:11 -0500

    started describe block

---
+++ tests/create-test.js
@@ -0,0 +1,10 @@
+// var expect = chai.expect;
+//
+// describe('create post form', function(){
+//   it('should trigger a create:post event on the document with the title and body', function(done){
+//     $(document).on('create:post', function(event, posts){
+//       expect().toBe();
+//       done();
+//     })
+//   });
+// });
--- tests/sample.js
+++ tests/sample.js
@@ -8,3 +8,12 @@ describe('hello', function(){
     expect(hello()).to.be.equal('hello world');
   });
 });
+
+describe('create post form', function(){
+  it('should trigger a create:post event on the document with the title and body', function(done){
+    $(document).on('create:post', function(event, posts){
+      expect().toBe();
+      done();
+    })
+  });
+});
